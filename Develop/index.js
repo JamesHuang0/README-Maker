cf3c12de3ae0bdd8c429319296821c74e94164bd
mrxanthic <mrxanthic@gmail.com>
2022-04-21 21:13:36 -0500

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -6,12 +6,50 @@ const inquirer = require("inquirer");
 const questions = () => {
     return inquirer.prompt([
         {
-            type: "input",
-            message: "What is your GitHub username?",
-            name: "username"
+            type: 'input',
+            name: 'title',
+            message: "What is your project's title?"
         },
         {
-
+            type: 'input',
+            name: 'description',
+            message: 'Give a description of your project.'
+        },
+        {
+            type: 'input',
+            name: 'instructions',
+            message: 'Give instructions on how to install your project.'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter usage information.'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Enter contribution guidelines.'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Enter test instructions.'
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'Select which license the application is covered under.',
+            choices: ['', '', '', '', '', '', '']
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "What is your GitHub username?"
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?'
         }
     ])}; 
 
