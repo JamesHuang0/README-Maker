2e8744b8dd4b870fb1e5971e23fee0c78a3ab2b7
mrxanthic <mrxanthic@gmail.com>
2022-04-21 20:52:55 -0500

    added packages and questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "username"
+        },
+        {
+
+        }
+    ])}; 
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
